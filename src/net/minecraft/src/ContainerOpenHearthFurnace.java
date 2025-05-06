b65667e50282ab43642b73b699a1713a904624c4
rahulinmaksym <rahulinmaksym@gmail.com>
2025-05-06 16:34:15 +0300

    Initial commit

---
+++ src/net/minecraft/src/ContainerOpenHearthFurnace.java
@@ -0,0 +1,102 @@
+package net.minecraft.src;
+
+public class ContainerOpenHearthFurnace extends Container {
+
+    private TileEntityOpenHearthFurnace oHFurnace;
+    private int cookTime = 0;
+    private int burnTime = 0;
+    private int itemBurnTime = 0;
+
+    public ContainerOpenHearthFurnace(InventoryPlayer inventoryPlayer, TileEntityOpenHearthFurnace tileEntity) {
+        this.oHFurnace = tileEntity;
+        this.addSlot(new Slot(tileEntity, 0, 56, 17));
+        this.addSlot(new Slot(tileEntity, 1, 56, 53));
+        this.addSlot(new SlotOpenHearthFurnace(inventoryPlayer.player, oHFurnace, 2, 116, 35));
+
+        int var3;
+        for(var3 = 0; var3 < 3; ++var3) {
+            for(int var4 = 0; var4 < 9; ++var4) {
+                this.addSlot(new Slot(inventoryPlayer, var4 + var3 * 9 + 9, 8 + var4 * 18, 84 + var3 * 18));
+            }
+        }
+
+        for(var3 = 0; var3 < 9; ++var3) {
+            this.addSlot(new Slot(inventoryPlayer, var3, 8 + var3 * 18, 142));
+        }
+
+    }
+
+    public void updateCraftingResults() {
+        super.updateCraftingResults();
+
+        for(int var1 = 0; var1 < this.field_20121_g.size(); ++var1) {
+            ICrafting var2 = (ICrafting)this.field_20121_g.get(var1);
+            if(this.cookTime != this.oHFurnace.furnaceCookTime) {
+                var2.func_20158_a(this, 0, this.oHFurnace.furnaceCookTime);
+            }
+
+            if(this.burnTime != this.oHFurnace.furnaceBurnTime) {
+                var2.func_20158_a(this, 1, this.oHFurnace.furnaceBurnTime);
+            }
+
+            if(this.itemBurnTime != this.oHFurnace.currentItemBurnTime) {
+                var2.func_20158_a(this, 2, this.oHFurnace.currentItemBurnTime);
+            }
+        }
+
+        this.cookTime = this.oHFurnace.furnaceCookTime;
+        this.burnTime = this.oHFurnace.furnaceBurnTime;
+        this.itemBurnTime = this.oHFurnace.currentItemBurnTime;
+    }
+
+    public void func_20112_a(int var1, int var2) {
+        if(var1 == 0) {
+            this.oHFurnace.furnaceCookTime = var2;
+        }
+
+        if(var1 == 1) {
+            this.oHFurnace.furnaceBurnTime = var2;
+        }
+
+        if(var1 == 2) {
+            this.oHFurnace.currentItemBurnTime = var2;
+        }
+
+    }
+
+    public boolean isUsableByPlayer(EntityPlayer var1) {
+        return this.oHFurnace.canInteractWith(var1);
+    }
+
+    public ItemStack getStackInSlot(int var1) {
+        ItemStack var2 = null;
+        Slot var3 = (Slot)this.slots.get(var1);
+        if(var3 != null && var3.getHasStack()) {
+            ItemStack var4 = var3.getStack();
+            var2 = var4.copy();
+            if(var1 == 2) {
+                this.func_28125_a(var4, 3, 39, true);
+            } else if(var1 >= 3 && var1 < 30) {
+                this.func_28125_a(var4, 30, 39, false);
+            } else if(var1 >= 30 && var1 < 39) {
+                this.func_28125_a(var4, 3, 30, false);
+            } else {
+                this.func_28125_a(var4, 3, 39, false);
+            }
+
+            if(var4.stackSize == 0) {
+                var3.putStack((ItemStack)null);
+            } else {
+                var3.onSlotChanged();
+            }
+
+            if(var4.stackSize == var2.stackSize) {
+                return null;
+            }
+
+            var3.onPickupFromSlot(var4);
+        }
+
+        return var2;
+    }
+}
---
+++ src/net/minecraft/src/SlotOpenHearthFurnace.java
@@ -0,0 +1,31 @@
+package net.minecraft.src;
+
+public class SlotOpenHearthFurnace extends Slot {
+
+    private EntityPlayer thePlayer;
+
+    public SlotOpenHearthFurnace(EntityPlayer player, IInventory var2, int var3, int var4, int var5) {
+        super(var2, var3, var4, var5);
+        this.thePlayer = player;
+    }
+
+    public boolean isItemValid(ItemStack var1) {
+        return false;
+    }
+
+    public void onPickupFromSlot(ItemStack var1) {
+        var1.onCrafting(this.thePlayer.worldObj, this.thePlayer);
+
+        /*
+        if(var1.itemID == Item.ingotIron.shiftedIndex) {
+            this.thePlayer.addStat(AchievementList.acquireIron, 1);
+        }
+
+        if(var1.itemID == Item.fishCooked.shiftedIndex) {
+            this.thePlayer.addStat(AchievementList.cookFish, 1);
+        }
+         */
+
+        super.onPickupFromSlot(var1);
+    }
+}
